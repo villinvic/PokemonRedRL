f90626e34860a4653628b0d11e2bae4ef47cfb49
villinvic <vvillin@hotmail.fr>
2023-10-28 12:29:41 +0200

    revamp

--- pkmn_rllib/run_experiment.py
+++ pkmn_rllib/run_experiment.py
@@ -62,7 +62,7 @@ config = VmpoConfig().training(
     grad_clip=10.,
     opt_type="rmsprop",
     train_batch_size=8192*4,
-    gamma=0.996,
+    gamma=0.997,
     model={
         "custom_model": "pokemon_lstm_model",
         "conv_filters": [
