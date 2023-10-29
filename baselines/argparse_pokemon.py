e5a32cb2401e4b9e8b24925cd548d0208c185fc7
villinvic <vvillin@hotmail.fr>
2023-10-29 09:18:45 +0100

    revamp

--- baselines/argparse_pokemon.py
+++ baselines/argparse_pokemon.py
@@ -26,8 +26,8 @@ def get_args(usage_string=None, ep_length=None, sess_path=None, headless=True):
     parser.add_argument('--gb_path', type=str, default='PokemonRed.gb', help='Path to the gameboy ROM')
     parser.add_argument('--debug', type=bool, default=False, help='Whether to run the environment in debug mode')
     # 40_000_000.0
-    parser.add_argument('--sim_frame_dist', type=float, default=40_000_000.0, help='Simulation frame distance')
-    args, unknown_args= parser.parse_known_args() # Parses only the known args to fix an issue with argv[1] being used as a save path
+    parser.add_argument('--sim_frame_dist', type=float, default=30_000_000.0, help='Simulation frame distance')
+    args, unknown_args = parser.parse_known_args() # Parses only the known args to fix an issue with argv[1] being used as a save path
     return args
 
 def change_env(env_config, args):
