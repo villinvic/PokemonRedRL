985af0e10e872a8da93ad15b2d8e8ede7a6da3e2
villinvic <vvillin@hotmail.fr>
2023-10-31 23:14:39 +0100

    revamp

--- pkmn_env/red.py
+++ pkmn_env/red.py
@@ -497,7 +497,7 @@ class PkmnRedEnv(Env):
                     (self.game_stats[PkmnRedEnv.PARTY_EXPERIENCE][-1][i]
                     - self.game_stats[PkmnRedEnv.PARTY_EXPERIENCE][-2][i]) * int(self.game_stats[PkmnRedEnv.PARTY_EXPERIENCE][-2][i] == 0.)
                     , 0.
-                ) / np.maximum(max(self.game_stats[PkmnRedEnv.PARTY_LEVELS][-1])**3,
+                ) / np.maximum(max(self.game_stats[PkmnRedEnv.PARTY_LEVELS][-1], 1.)**3,
                 )
 
             rewards.update(**{
