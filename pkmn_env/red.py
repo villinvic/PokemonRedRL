792227dd948cf92a1a52666a32db7b8fee4e7115
villinvic <vvillin@hotmail.fr>
2023-10-31 20:16:54 +0100

    revamp

--- pkmn_env/red.py
+++ pkmn_env/red.py
@@ -215,7 +215,7 @@ class PkmnRedEnv(Env):
         self.reward_function_config = {
             PkmnRedEnv.BLACKOUT         :   0.,
             PkmnRedEnv.SEEN_POKEMONS    :   0.,
-            PkmnRedEnv.TOTAL_EXPERIENCE :   0.5,  # 0.5
+            PkmnRedEnv.TOTAL_EXPERIENCE :   2,  # 0.5
             PkmnRedEnv.BADGE_SUM        :   100.,
             PkmnRedEnv.MAPS_VISITED     :   5,
 
