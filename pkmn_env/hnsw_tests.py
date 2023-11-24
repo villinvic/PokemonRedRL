5764f630d384cabf1859478246643c5f92eb573f
villinvic <vvillin@hotmail.fr>
2023-11-24 23:52:10 +0100

    novelty tests

---
+++ pkmn_env/hnsw_tests.py
@@ -0,0 +1,75 @@
+from datetime import time
+
+import hnswlib
+import numpy as np
+import os
+import cv2
+dim = 32
+num_elements = 100000
+k = 1
+num_queries = 32
+
+# Generating sample data
+data = []
+
+_, _, files = next(os.walk("/home/goji/Documents/PokemonRedRL/sessions/session_4a4b0522/novelty_frames"))
+for i, file in enumerate(files):
+    if "observed" in file:
+        img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+        data.append(img.flatten()[np.newaxis])
+    print(i, len(files))
+
+data = np.concatenate(data, axis=0)
+
+# Declaring index
+hnsw_index = hnswlib.Index(space='l2', dim=len(data[0]))  # possible options are l2, cosine or ip
+bf_index = hnswlib.BFIndex(space='l2', dim=len(data[0]))
+
+# Initing both hnsw and brute force indices
+# max_elements - the maximum number of elements (capacity). Will throw an exception if exceeded
+# during insertion of an element.
+# The capacity can be increased by saving/loading the index, see below.
+#
+# hnsw construction params:
+# ef_construction - controls index search speed/build speed tradeoff
+#
+# M - is tightly connected with internal dimensionality of the data. Strongly affects the memory consumption (~M)
+# Higher M leads to higher accuracy/run_time at fixed ef/efConstruction
+
+hnsw_index.init_index(max_elements=num_elements, ef_construction=200, M=16)
+bf_index.init_index(max_elements=num_elements)
+
+# Controlling the recall for hnsw by setting ef:
+# higher ef leads to better accuracy, but slower search
+hnsw_index.set_ef(200)
+
+# Set number of threads used during batch search/construction in hnsw
+# By default using all available cores
+hnsw_index.set_num_threads(1)
+
+print("Adding batch of %d elements" % (len(data)))
+hnsw_index.add_items(data)
+bf_index.add_items(data)
+
+print("Indices built")
+
+# Generating query data
+query_data = data[np.random.choice(len(data), num_queries, replace=False)]
+
+
+# Query the elements and measure recall:
+labels_hnsw, distances_hnsw = hnsw_index.knn_query(query_data, k)
+labels_bf, distances_bf = bf_index.knn_query(query_data, k)
+
+# Measure recall
+correct = 0
+t = time()
+for i in range(num_queries):
+    for label in labels_hnsw[i]:
+        for correct_label in labels_bf[i]:
+            if label == correct_label:
+                correct += 1
+                break
+
+print("recall is :", float(correct)/(k*nun_queries))
+print("time required:", time() - t)
