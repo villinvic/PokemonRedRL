6f2b6e982194864190065a0131c33ce3eeba59d7
villinvic <vvillin@hotmail.fr>
2023-11-25 00:12:04 +0100

    novelty tests

--- pkmn_env/hnsw_tests.py
+++ pkmn_env/hnsw_tests.py
@@ -11,12 +11,14 @@ parser.add_argument('--ef', type=int, default=200)
 parser.add_argument('--ef_construction', type=int, default=200)
 parser.add_argument('--m', type=int, default=16)
 parser.add_argument('--n_queries', type=int, default=64)
+parser.add_argument('--k', type=int, default=1)
+
 
 
 args, unknown_args = parser.parse_known_args()  # Parses only the known args to fix an issue with argv[1] being used as a save path
 print(args)
 num_elements = 100000
-k = 3
+k = args.k
 num_queries = args.n_queries
 
 
