ce8b629645cdd6d742caa65ad112259f111ff3ab
villinvic <vvillin@hotmail.fr>
2023-11-27 12:57:11 +0100

    novelty tests

--- pkmn_rllib/rllib/vmpo/rllib_callbacks.py
+++ pkmn_rllib/rllib/vmpo/rllib_callbacks.py
@@ -31,7 +31,7 @@ class PokemonCallbacks(
         self.width = None
         self.height = None
         self.height_cut = None
-        self.similar_frame_dist = 4000.
+        self.similar_frame_dist = 4500.
         self.path = Path("sessions/novelty_frames")
         self.path.mkdir(parents=True, exist_ok=True)
         self.num_distinct_frames = 0
@@ -106,8 +106,9 @@ class PokemonCallbacks(
                             self.num_distinct_frames += 1
                             print(self.num_distinct_frames, d)
 
-                            if self.num_distinct_frames > 1000:
+                            if self.num_distinct_frames > 2000:
                                 screenshot_path = self.path / Path(f"{self.num_distinct_frames}_{int(d)}.jpeg")
+                                print(screenshot_path)
                                 cv2.imwrite(screenshot_path.as_posix(), screen[:,:,0])
                                 train_batch[SampleBatch.REWARDS][idx] += 30.
                                 total_novelty += 1
