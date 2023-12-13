bd31c900fc82097fafd56a9d4b2fcfa0b9b70474
villinvic <vvillin@hotmail.fr>
2023-12-13 16:16:45 +0100

    ICM

--- pkmn_rllib/rllib/vmpo/VmpoPolicy.py
+++ pkmn_rllib/rllib/vmpo/VmpoPolicy.py
@@ -237,16 +237,19 @@ class VmpoPolicy(
             state_prediction_loss = self.model.state_prediction_loss()
             action_prediction_loss = self.model.action_prediction_loss()
 
+            self.mean_state_prediction_loss = tf.reduce_mean(state_prediction_loss)
+            self.mean_action_prediction_loss = tf.reduce_mean(action_prediction_loss)
+
             intrinsic_rewards = tf.reduce_mean(state_prediction_loss * self.model.curiosity_reward_scale, axis=1, keepdims=False)
             rewards = rewards + tf.stop_gradient(intrinsic_rewards)
 
             self.mean_intrinsic_rewards = tf.reduce_mean(intrinsic_rewards)
-            icm_loss = ((action_prediction_loss * (1. - self.model.icm_beta) + state_prediction_loss * self.model.icm_beta)
+            icm_loss = ((self.mean_action_prediction_loss * (1. - self.model.icm_beta)
+                         + self.mean_state_prediction_loss * self.model.icm_beta)
                         / self.model.icm_lambda)
 
-            self.mean_icm_loss = tf.reduce_mean(icm_loss)
-            self.mean_state_prediction_loss = tf.reduce_mean(state_prediction_loss)
-            self.mean_action_prediction_loss = tf.reduce_mean(action_prediction_loss)
+            self.mean_icm_loss = icm_losl
+
         else:
 
             self.mean_intrinsic_rewards = tf.zeros((1,), dtype=tf.float32)
