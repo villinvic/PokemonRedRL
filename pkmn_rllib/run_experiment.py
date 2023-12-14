72ab2d7757f988df072a118bcb3bae482519cf97
villinvic <vvillin@hotmail.fr>
2023-12-14 03:57:24 +0100

    ICM

--- pkmn_rllib/run_experiment.py
+++ pkmn_rllib/run_experiment.py
@@ -80,7 +80,7 @@ config = VmpoConfig().training(
         "conv_filters": [
             [32, [4, 4], 3, "same"],
             [32, [4, 4], 3, "same"],
-            [32, [4, 4], 3, "same"],
+            [32, [4, 4], 2, "same"],
             [32, [3, 3], 2, "same"],
         ],
         "fcnet_size": 256,
