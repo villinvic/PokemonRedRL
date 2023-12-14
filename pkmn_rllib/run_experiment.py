d1183a736a2a9069970017831b164d885215555a
villinvic <vvillin@hotmail.fr>
2023-12-14 03:59:06 +0100

    ICM

--- pkmn_rllib/run_experiment.py
+++ pkmn_rllib/run_experiment.py
@@ -78,8 +78,8 @@ config = VmpoConfig().training(
     model={
         "custom_model": "pokemon_icm_model",
         "conv_filters": [
-            [32, [4, 4], 3, "same"],
-            [32, [4, 4], 3, "same"],
+            [32, [3, 3], 2, "same"],
+            [32, [3, 3], 2, "same"],
             [32, [3, 3], 2, "same"],
             [32, [3, 3], 2, "same"],
         ],
