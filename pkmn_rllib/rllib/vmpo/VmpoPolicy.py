e7b2896546632b83bd1995b6e7a5b9ac0bfd7030
villinvic <vvillin@hotmail.fr>
2023-12-15 11:37:55 +0100

    ICM

--- pkmn_rllib/rllib/vmpo/VmpoPolicy.py
+++ pkmn_rllib/rllib/vmpo/VmpoPolicy.py
@@ -605,7 +605,7 @@ class VmpoPolicy(
         if results:
 
             idx = self.global_timestep % 10
-            pil_img = tf.keras.preprocessing.image.array_to_img(self.surprising_state)
+            pil_img = tf.keras.preprocessing.image.array_to_img(self.get_session().run(self.surprising_state))
             path = Path(f"debug/surprise/surprising_image_{idx}.png")
             path.mkdir(parents=True, exist_ok=True)
             pil_img.save(path)
