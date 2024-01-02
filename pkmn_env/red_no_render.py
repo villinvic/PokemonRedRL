a84d80ca0e578d684ade0691943785492b686b68
villinvic <vvillin@hotmail.fr>
2024-01-02 22:05:46 +0100

    done jobs

--- pkmn_env/red_no_render.py
+++ pkmn_env/red_no_render.py
@@ -105,7 +105,7 @@ class PkmnRedEnvNoRender(Env):
 
         self.pyboy = PyBoy(
             config['gb_path'],
-            disable_input=True,
+            disable_input=False,
             disable_renderer=not config["render"],
             hide_window=not config["render"],
             window_type="SDL2" if config["render"] else "headless",
